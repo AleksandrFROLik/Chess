5bc1790e08f9ea0ec6b89d1ac4e7c9c9915cfeac
AleksandrFROLik <nextfrolik@gmail.com>
2022-05-31 23:49:18 +0300

    path 1 board is ready

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export const App = () => {
 
   return (
     <div className="app">
-      <BoardComponent/>
+      <BoardComponent board={board} setBoard={setBoard}/>
     </div>
   )
 };
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Board } from '../models/Board';
+import { CellComponent } from '../components/CellComponent';
 
-export const BoardComponent = () => {
+interface BoardProps {
+  board: Board;
+  setBoard: (board: Board) => void
+}
+
+export const BoardComponent: FC<BoardProps> = ({board,setBoard}) => {
   return (
-    <div className='board'>
-
+    <div className="board">
+      {board.cells.map((row, index)=>
+        <React.Fragment key={index}>
+          {row.map(cell =>
+            <CellComponent cell={cell} key={cell.id}/>
+          )}
+        </React.Fragment>
+        )}
     </div>
   );
 };
