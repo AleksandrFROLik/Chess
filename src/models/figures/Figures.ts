e16501b5f326fbc83a8c255e51698de32e965691
AleksandrFROLik <nextfrolik@gmail.com>
2022-06-14 21:50:28 +0300

    path 2 add figures

--- src/models/figures/Figures.ts
+++ src/models/figures/Figures.ts
@@ -1,3 +1,38 @@
-export class Figures {
+import logo from '../../assets/black-king.png'
+import { Colors } from '../../models/Colors';
+import { Cell } from '../Cell';
 
+export enum FigureNames {
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  KNIGHT = 'Конь',
+  PAWN = 'пешка',
+  QUEEN = 'Ферзь',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
+}
+
+export class Figures {
+  color: Colors;
+  logo: typeof logo | null;
+  cell: Cell;
+  name: FigureNames;
+  id: number;
+
+
+  constructor( color: Colors, cell: Cell ) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE;
+    this.id = Math.random()
+  }
+
+  canMove( target: Cell ): boolean {
+    return  true
+  }
+  moveFigure(target: Cell) {
+
+  }
 }
