3dcb202a4610f1eebaa1485930f0a16f82b5c385
AleksandrFROLik <nextfrolik@gmail.com>
2022-06-16 22:12:05 +0300

    path 2 add figures++

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,3 +1,13 @@
-export  class Player {
+import { Colors } from 'src/models/Colors';
+import { Figures } from 'src/models/figures/Figures';
+import { Board } from 'src/models/Board';
 
+export  class Player {
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  figure: Figures | null;
+  board: Board;
+  available: boolean;
+  id: number;
 }
